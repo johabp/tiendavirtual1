c35a590aaecdb0a73e557c6ea2498f970c68ee11
debernall <debernall@unal.edu.co>
2021-09-10 20:10:58 -0500

    Cambios

---
+++ tiendavirtual1/src/main/java/co/edu/unbosque/tiendavirtual1/Tiendavirtual1Application.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.tiendavirtual1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tiendavirtual1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tiendavirtual1Application.class, args);
+	}
+
+}
---
+++ tiendavirtual1/src/test/java/co/edu/unbosque/tiendavirtual1/Tiendavirtual1ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.tiendavirtual1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tiendavirtual1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
